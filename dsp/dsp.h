1564858318556b8bb96fd9d852334e457beb6905
sctanf <36978460+sctanf@users.noreply.github.com>
2025-11-06 14:46:49 -0600

    optimize biquad processing

--- dsp/dsp.h
+++ dsp/dsp.h
@@ -1,8 +1,6 @@
 #ifndef PICO_AMP_DSP_H
 #define PICO_AMP_DSP_H
 
-#define USE_FIX16_MUL
-
 #include <stdio.h>
 
 #include "pico/stdlib.h"
@@ -32,36 +30,8 @@ typedef int32_t dspfx;
 //#define mulshift(a) ((dspfx)fxint3(a))
 #define floatfx3(a) ((int64_t)((a)*(1<<fpformat3)))
 
-#ifdef USE_FIX16_MUL
-typedef int32_t fix3_28_t;
-
 // https://github.com/ploopyco/headphones/blob/master/firmware/code/fix16.inl
-static inline fix3_28_t fix16_mul(fix3_28_t inArg0, fix3_28_t inArg1) {
-    int32_t A = (inArg0 >> 14), C = (inArg1 >> 14);
-    uint32_t B = (inArg0 & 0x3FFF), D = (inArg1 & 0x3FFF);
-    int32_t AC = A*C;
-    int32_t AD_CB = A*D + C*B;
-    int32_t product_hi = AC + (AD_CB >> 14);
-
-#if HANDLE_CARRY
-	// Handle carry from lower bits to upper part of result.
-    uint32_t BD = B*D;
-	uint32_t ad_cb_temp = AD_CB << 14;
-	uint32_t product_lo = BD + ad_cb_temp;
-
-	if (product_lo < BD)
-		product_hi++;
-#endif
-
-    return product_hi;
-}
-
-#define mulfx0(a,b) fix16_mul(a,b)
-#define mulshift(a) (a)
-#else
-#define mulfx0(a,b) ((int64_t)(a)*(int64_t)(b))
-#define mulshift(a) ((dspfx)fxint3(a))
-#endif
+#define mulfxHL(bH, bL, a) ((a >> 14) * bH + (((a >> 14) * bL + bH * (a & 0x3FFF)) >> 14))
 
 typedef struct {
 	dspfx a1z, a2z, b1z, b2z;
@@ -69,17 +39,18 @@ typedef struct {
 } biquad;
 
 #define biquad(a) biquad a = {.a1z=0,.a2z=0,.b1z=0,.b2z=0,.a1zr=0,.a2zr=0,.b1zr=0,.b2zr=0};
-#define biquadconstfx(a,b,c,d,e) floatfx3(a),floatfx3(b),floatfx3(c),floatfx3(d),floatfx3(e)
+#define biqconstfxHL(a) (floatfx3(a) >> 14),(floatfx3(a) & 0x3FFF)
+#define biquadconstfx(a, b, c, d, e) biqconstfxHL(a), biqconstfxHL(b), biqconstfxHL(c), biqconstfxHL(d), biqconstfxHL(e)
 #define biquadconstsfx(a) biquadconstfx(a)
 
-static inline void process_biquad(biquad *const filter, int64_t a0, int64_t a1, int64_t a2, int64_t b1, int64_t b2, int16_t iters, int32_t *in, int32_t *out) {
+static inline void process_biquad(biquad *const filter, int32_t a0H, int32_t a0L, int32_t a1H, int32_t a1L, int32_t a2H, int32_t a2L, int32_t b1H, int32_t b1L, int32_t b2H, int32_t b2L, int16_t iters, int32_t *in, int32_t *out) {
 	int16_t iters2 = iters * 2;
-	out[0] = mulshift(mulfx0(a0, in[0]) + mulfx0(a1, filter->a1z) - mulfx0(b1, filter->b1z) + mulfx0(a2, filter->a2z) - mulfx0(b2, filter->b2z));
-	out[2] = mulshift(mulfx0(a0, in[2]) + mulfx0(a1, in[0]) - mulfx0(b1, out[0]) + mulfx0(a2, filter->a1z) - mulfx0(b2, filter->b1z));
-	out[1] = mulshift(mulfx0(a0, in[1]) + mulfx0(a1, filter->a1zr) - mulfx0(b1, filter->b1zr) + mulfx0(a2, filter->a2zr) - mulfx0(b2, filter->b2zr));
-	out[3] = mulshift(mulfx0(a0, in[3]) + mulfx0(a1, in[1]) - mulfx0(b1, out[1]) + mulfx0(a2, filter->a1zr) - mulfx0(b2, filter->b1zr));
+	out[0] = mulfxHL(a0H, a0L, in[0]) + mulfxHL(a1H, a1L, filter->a1z)  - mulfxHL(b1H, b1L, filter->b1z)  + mulfxHL(a2H, a2L, filter->a2z)  - mulfxHL(b2H, b2L, filter->b2z);
+	out[2] = mulfxHL(a0H, a0L, in[2]) + mulfxHL(a1H, a1L, in[0])        - mulfxHL(b1H, b1L, out[0])       + mulfxHL(a2H, a2L, filter->a1z)  - mulfxHL(b2H, b2L, filter->b1z);
+	out[1] = mulfxHL(a0H, a0L, in[1]) + mulfxHL(a1H, a1L, filter->a1zr) - mulfxHL(b1H, b1L, filter->b1zr) + mulfxHL(a2H, a2L, filter->a2zr) - mulfxHL(b2H, b2L, filter->b2zr);
+	out[3] = mulfxHL(a0H, a0L, in[3]) + mulfxHL(a1H, a1L, in[1])        - mulfxHL(b1H, b1L, out[1])       + mulfxHL(a2H, a2L, filter->a1zr) - mulfxHL(b2H, b2L, filter->b1zr);
 	for (int i = 4; i < iters2; i++) {
-		out[i] = mulshift(mulfx0(a0, in[i]) + mulfx0(a1, in[i - 2]) - mulfx0(b1, out[i - 2]) + mulfx0(a2, in[i - 4]) - mulfx0(b2, out[i - 4])); // takes up the most time by far..
+		out[i] = mulfxHL(a0H, a0L, in[i]) + mulfxHL(a1H, a1L, in[i - 2]) - mulfxHL(b1H, b1L, out[i - 2]) + mulfxHL(a2H, a2L, in[i - 4]) - mulfxHL(b2H, b2L, out[i - 4]); // takes up the most time by far..
 	}
 	filter->a2z = in[iters2 - 4];
 	filter->b2z = out[iters2 - 4];
